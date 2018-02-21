98a89a5c0c5b706e61f0fa0728dc38890673d829
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-21 18:50:22 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -1,7 +1,13 @@
 def using_times
 
 7.times do
+<<<<<<< HEAD
     puts "Wingardium Leviosa"
+=======
+  loop
+    puts "Wingardium Leviosa"
+  break
+>>>>>>> 8817869515f2cb10f03eaa5c095b217db3a017e8
 end
 end
 
